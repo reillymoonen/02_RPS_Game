a193f8595a3e2e05ae5dbe148f58b6710bbdc99b
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-06-27 11:51:26 +1200

    Update 02_user_choice_v1.py

--- 02_user_choice_v1.py
+++ 02_user_choice_v1.py
@@ -1,9 +1,21 @@
 # Functions go here
+def choice_checker(question):
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice
+        responce = input(question)
+
+        if responce == "r" or responce == "rock":
+            return responce
 
 
 # Main routine goes here
 
 # Ask user for choice and check it's valid
+user_choice = choice_checker("Choose rock / paper/ "
+                             "scissors (r/p/s): ")
 
 
 # Print out choice for comparison purposes
