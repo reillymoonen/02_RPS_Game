ee8edb60fac3db76b88798fda2738c490ab3fdfb
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-07-18 12:13:37 +1200

    Update 01_RPS_Base_Component.py

---
+++ 01_RPS_Base_Component.py
@@ -0,0 +1,13 @@
+import random
+
+# Functions go here
+
+
+# Main routine goes here
+
+# Lists of valid responses
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+# Ask user if they have played before
+# If 'no', show instructions
